0b5785e94d1e1bcbb6eeb0564f71d25ee3a2bb82
Rejah Anuvar <rejahanwer@gmail.com>
2021-05-14 16:10:48 +0530

    Basics of UI.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,9 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isShowingAlert = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack(alignment: .center) {
+            //LinearGradient(gradient: Gradient(colors: [.red, .gray]), startPoint: .top, endPoint: .bottom)
+            
+            RadialGradient(gradient: Gradient(colors: [.red, .black]),
+                           center: .center,
+                           startRadius: 10,
+                           endRadius: 500)
+
+            VStack(alignment: .center) {
+
+                Button(action: {
+                    self.isShowingAlert = true
+                }) {
+                    HStack {
+                        Image(systemName: "pencil").renderingMode(.original)
+                        Text("Edit")
+                    }
+                }
+                .alert(isPresented: $isShowingAlert, content: {
+                    Alert(title: Text("Hello SwiftUI!"), message: Text("This is some detail message"), dismissButton: .default(Text("OK")))
+                })
+                
+                HStack() {
+                    Text("Hello, ")
+                    Text("world!")
+                    Text("Ahoy!")
+                }.padding().background(Color.green)
+                
+                HStack() {
+                    Text("Hello, ")
+                    Text("world!")
+                    Text("Ahoy!")
+                }.padding().background(Color.green)
+                
+                HStack() {
+                    Text("Hello, ")
+                    Text("world!")
+                    Text("Ahoy!")
+                }.padding().background(Color.green)
+            }
+        }.background(Color.red)
     }
 }
 
